3d48fb0f3bfb1a392b7868adaa1d14c053b65af3
Mikhail <mr.nimostor@mail.ru>
2023-02-16 14:06:56 +0300

    setting up

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
@@ -7,27 +5,22 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
     vueJsx(),
     Components({
-      dirs:['src/components'],
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue'],
-      deep: true,
-      // allow auto import and register components used in markdown
       resolvers: [
         ElementPlusResolver(),
       ],
-      dts: true,
-      include: [/\.vue$/, /\.vue\?vue/,],
     }),
   ],
   resolve: {
     alias: {
-      '~/': fileURLToPath(new URL('./src', import.meta.url))
+      '~/': path.resolve(__dirname, './src')
     }
   }
 })
