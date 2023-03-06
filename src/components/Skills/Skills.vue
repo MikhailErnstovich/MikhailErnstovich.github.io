adf04350447abab852e2158ed153c60f53fb167f
Mikhail <mr.nimostor@mail.ru>
2023-03-06 19:48:59 +0300

    skill component

---
+++ src/components/Skills/Skills.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="skills">
+    <ul class="skills__list">
+      <li class="skills__list-item" v-for="item in skills">{{ item }}</li>
+    </ul>
+  </article>
+</template>
+
+<script setup lang="tsx">
+import { skills } from './skill-data';
+
+</script>
+
+<style  lang="scss">
+.skills {
+  &__list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    gap: 0 10px;
+    list-style: none;
+    &-item {
+      position: relative;
+      padding-left: 20px;
+      height: 30px;
+      line-height: 30px;
+      font-size: var(--fz-sm);
+      &::before {
+        display: block;
+        content: '⚙︎';
+        position: absolute;
+        left: 0;
+        height: inherit;
+        line-height: inherit;
+        color: var(--bays-0);
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Skills/skill-data.ts
@@ -0,0 +1,8 @@
+export const skills: string[] = [
+  'JavaScript (ES6+)',
+  'Vue 2/3',
+  'TypeScript',
+  'SCSS',
+  'npm',
+  'Webpack',
+];
