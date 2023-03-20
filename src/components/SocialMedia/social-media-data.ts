03650f8f7b9a0caab9b137f6a5cdbef82a428dde
Mikhail <mr.nimostor@mail.ru>
2023-03-20 15:45:45 +0300

    Icon links

--- src/components/SocialMedia/social-media-data.ts
+++ src/components/SocialMedia/social-media-data.ts
@@ -3,27 +3,27 @@ export const contacts = {
     {
       link: 'https://www.researchgate.net/profile/Mikhail-Makarov-6',
       title: 'ResearchGate',
-      img: '/src/assets/icons/research-gate.svg',
+      img: 'https://github.com/MikhailErnstovich/MikhailErnstovich.github.io/blob/master/src/assets/icons/research-gate.svg',
     },
     {
       link: 'https://www.linkedin.com/in/mikhail-makarov-639a55164/',
       title: 'LinkedIn',
-      img: '/src/assets/icons/linkedin.svg',
+      img: 'https://raw.githubusercontent.com/MikhailErnstovich/MikhailErnstovich.github.io/master/src/assets/icons/linkedin.svg',
     },
     {
       link: 'https://github.com/MikhailErnstovich',
       title: 'GitHub',
-      img: '/src/assets/icons/github.svg',
+      img: 'https://raw.githubusercontent.com/MikhailErnstovich/MikhailErnstovich.github.io/master/src/assets/icons/github.svg',
     },
     {
       link: 'https://t.me/R0b1nH00d',
       title: 'Telegram',
-      img: '/src/assets/icons/telegram.svg',
+      img: 'https://raw.githubusercontent.com/MikhailErnstovich/MikhailErnstovich.github.io/master/src/assets/icons/telegram.svg',
     },
   ],
   email: {
     link: 'mailto: mikhail.makarov@phystech.edu',
     title: 'E-mail',
-    img: '/src/assets/icons/mail.svg',
+    img: 'https://raw.githubusercontent.com/MikhailErnstovich/MikhailErnstovich.github.io/master/src/assets/icons/mail.svg',
   }
 };
