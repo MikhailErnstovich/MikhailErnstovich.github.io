d992b27eb7afee63bca56c1c623f295baba72e1e
Mikhail <mr.nimostor@mail.ru>
2023-03-20 15:47:49 +0300

    research gate icon

--- src/components/SocialMedia/social-media-data.ts
+++ src/components/SocialMedia/social-media-data.ts
@@ -3,7 +3,7 @@ export const contacts = {
     {
       link: 'https://www.researchgate.net/profile/Mikhail-Makarov-6',
       title: 'ResearchGate',
-      img: 'https://github.com/MikhailErnstovich/MikhailErnstovich.github.io/blob/master/src/assets/icons/research-gate.svg',
+      img: 'https://raw.githubusercontent.com/MikhailErnstovich/MikhailErnstovich.github.io/master/src/assets/icons/research-gate.svg',
     },
     {
       link: 'https://www.linkedin.com/in/mikhail-makarov-639a55164/',
