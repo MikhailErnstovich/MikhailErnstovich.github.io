a2316909b64b9901686f4c9b608d06dbb39a940f
Mikhail <mr.nimostor@mail.ru>
2023-03-28 14:50:28 +0300

    lazy loading of photo

--- src/components/Photo/Photo.vue
+++ src/components/Photo/Photo.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="photo">
     <figure class="photo__wrapper">
-      <picture class="photo__content">
+      <picture v-inserted class="photo__content">
         <source 
-          :srcset="imgUrls.lg"
+          :data-url="imgUrls.lg"
           media="(min-width: 1000px)"
         />
         <source 
-          :srcset="imgUrls.md" 
+          :data-url="imgUrls.md"
           media="(min-width: 600px)"
         />
         <img 
           class="photo__img"
-          :src="imgUrls.sm"
+          :data-url="imgUrls.sm"
           alt="Mikhail Makarov"
         />
       </picture>
@@ -25,6 +25,65 @@ const imgUrls = {
   md:  new URL('~/assets/images/me-md.webp', import.meta.url).href,
   lg:  new URL('~/assets/images/me.webp', import.meta.url).href
 }
+
+const vInserted = {
+  mounted: (el: HTMLPictureElement):void => {
+    const loadImage = () => {
+      const imgElements = Array.from(el.children).filter(el => {
+        return el.nodeName === 'IMG' || el.nodeName === 'SOURCE'
+      });
+
+      if (imgElements.length) {
+        (imgElements as HTMLImageElement[]).forEach(el => {
+          el.addEventListener('load', () => {
+            setTimeout(() => el.classList.add('loaded'), 100)
+          });
+          el.addEventListener('error', () => console.log('error'));
+          if (!el.dataset.url) {
+            return;
+          }
+          switch (el.nodeName) {
+            case 'IMG': {
+              el.src = el.dataset.url;
+              break;
+            }
+            case 'SOURCE': {
+              el.srcset = el.dataset.url;
+              break;
+            }
+          }
+        });
+      
+      }
+    };
+
+    const handleIntersect = (entries: IntersectionObserverEntry[], observer: IntersectionObserver):void => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          loadImage();
+          observer.unobserve(el);
+        }
+      });
+    };
+    
+    const createObserver = ():void  => {
+      const options = {
+        root: null,
+        threshold: 0
+      };
+      const observer = new IntersectionObserver(handleIntersect, options);
+      observer.observe(el);
+    }
+
+    if (!window['IntersectionObserver']) {
+      loadImage();
+    } else {
+      createObserver();
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .photo {
