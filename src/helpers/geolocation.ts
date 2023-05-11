dfc038cc5f810b8f861dc55a223c820554498146
Mikhail <mr.nimostor@mail.ru>
2023-05-11 17:05:52 +0300

    geolocation timeout

--- src/helpers/geolocation.ts
+++ src/helpers/geolocation.ts
@@ -1,7 +1,7 @@
 export default function handleGeolocation (
   successCallback: PositionCallback,
   errorCallback: PositionErrorCallback,
-  options: PositionOptions = { enableHighAccuracy: true }
+  options: PositionOptions = { enableHighAccuracy: true, timeout: 2000 }
 ) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
 }
