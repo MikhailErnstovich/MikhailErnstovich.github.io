6562f6e214e2e9e7fe55937ecbe16423eab66502
Mikhail <mr.nimostor@mail.ru>
2023-05-22 14:54:30 +0300

    custom select

---
+++ src/components/CustomSelect/CustomSelect.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="select" @blur="hideOptions" tabindex="100">
+    <div class="select__output" :class="isVisible ? 'open' : ''" @click="handleVisibility">
+      <span>{{ selectedOpt }}</span>
+    </div>
+    <ul class="select__list" v-show="isVisible">
+      <li 
+        class="select__option"
+        :class="option === selectedOpt ? 'selected' : ''"
+        v-for="option in props.options.list"
+        :value="option"
+        :key="option"
+        @click="() => select(option)"
+      >
+        {{ option }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup lang="tsx">
+import { onMounted, ref } from 'vue'
+import { SelectOptions } from '~/Types';
+
+const props = defineProps<{
+  options: SelectOptions,
+}>();
+const emit = defineEmits<{
+  (e: 'select', value: string):void
+}>();
+
+const selectedOpt = ref('');
+const select = (item: string) => {
+  selectedOpt.value = item; 
+  isVisible.value = false;
+  emit('select', selectedOpt.value);
+};
+const isVisible = ref(false);
+const handleVisibility = () => isVisible.value = !isVisible.value;
+const hideOptions = () => isVisible.value = false;
+onMounted(() => selectedOpt.value = props.options.default);
+
+</script>
+<style lang="scss" scoped>
+.select {
+  display: block;
+  position: relative;
+  font-size: clamp(var(--fz-md), 0.8786rem + 0.5178vw, var(--fz-h-xs));
+  line-height: clamp(var(--lh-md), 1.379rem + 0.518vw, var(--lh-xl));
+  max-width: 4rem;
+  color: var(--boulders-4);
+  user-select: none;
+  &__output {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    border-bottom: 2px solid var(--boulders-4);
+    height: var(--fz-h-sm);
+    padding: 0 0.3rem;
+    text-transform: uppercase;
+    &.open,
+    &:hover,
+    &:focus {
+      border-bottom: 2px solid var(--primary-color);
+    }
+    &::after {
+      content: url(./assets/icons/angle-down.svg);
+      display: block;
+      width: 1rem;
+      transform: scale(0.7);
+    }
+  }
+  &__list {
+    list-style-type: none;
+    width: max-content;
+  }
+  &__option {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.3rem;
+    background-color: #fff;
+    font-size: clamp(var(--fz-xs), 0.692rem + 0.249vw, var(--fz-md));
+    text-transform: uppercase;
+    &:hover,
+    &:focus {
+      background-color: var(--boulders-1);
+    }
+    &.selected::after {
+      content: url(./assets/icons/tick.svg);
+      display: block;
+      margin-left: 0.3rem;
+      width: 1rem;
+      transform: scale(0.7);
+    }
+  }
+}
+</style>
