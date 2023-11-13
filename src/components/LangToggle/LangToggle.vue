01662a8d865c4187adb229ebf1c3f0a68298b4b7
Mikhail Makarov <mikhail.makarov@phystech.edu>
2023-11-13 18:30:49 +0300

    new fonts

--- src/components/LangToggle/LangToggle.vue
+++ src/components/LangToggle/LangToggle.vue
@@ -12,9 +12,33 @@ const options = {
   default: localStorage.getItem('locale') || 'en',
 };
 const handleSelect = (lang: string) => {
-  i18n.global.locale.value = lang as 'en' | 'ru'
+  i18n.global.locale.value = lang as 'en' | 'ru';
   localStorage.setItem('locale', lang);
+  setFont(lang as 'en' | 'ru');
 };
+
+const setFont = (lang: 'en' | 'ru') => {
+  const root = document.documentElement;
+  switch (lang) {
+    case 'ru': 
+      root.style.setProperty('--font-regular', 'NunitoSans-regular, sans-serif');
+      root.style.setProperty('--font-bold', 'NunitoSans-bold, sans-serif');
+      root.style.setProperty('--font-light', 'NunitoSans-light, sans-serif');
+      root.style.setProperty('--font-medium', 'NunitoSans-medium, sans-serif');
+      break;
+    case 'en': 
+      root.style.setProperty('--font-regular', 'Intel-regular');
+      root.style.setProperty('--font-bold', 'Intel-bold');
+      root.style.setProperty('--font-light', 'Intel-light');
+      root.style.setProperty('--font-medium', 'Intel-medium');
+      break;
+    default: 
+      root.style.setProperty('--font-regular', 'Intel-regular');
+      root.style.setProperty('--font-bold', 'Intel-bold');
+      root.style.setProperty('--font-light', 'Intel-light');
+      root.style.setProperty('--font-medium', 'Intel-medium');
+  }
+}
 </script>
 <style lang="scss" scoped>
 
